175a02f57d9016eb0124e7f5a9408531db0c5145
Israel Fernandez <israelfernandez96@gmail.com>
2025-02-21 01:08:12 -0800

    Added DaisyUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,16 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <p class="bg-red-50">Hello world!</p>
+
+<button class="btn">Button</button>
+<button class="btn btn-neutral">Neutral</button>
+<button class="btn btn-primary">Primary</button>
+<button class="btn btn-secondary">Secondary</button>
+<button class="btn btn-accent">Accent</button>
+<button class="btn btn-ghost">Ghost</button>
+<button class="btn btn-link">Link</button>
+<button class="btn btn-square">
+  <span class="loading loading-spinner"></span>
+</button>
+
+<!-- To get more components like this: https://daisyui.com/components/button/ -->
