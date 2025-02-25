b8b6aa7b061fade01711ff32572e5a4a9bfecccd
Israel Fernandez <israelfernandez96@gmail.com>
2025-02-24 17:08:41 -0800

    Added prettier, and updated layout and page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
-<script>
-  let { children } = $props();
-  import "../app.css";
+<script lang="ts">
+  import "/src/app.css";
 </script>
+<nav class="navbar">
+  <span class="navbar-start"> Tier List Application </span>
+  <span class="navbar-end">
+      <a href="/login" class="btn btn-outline">Login</a>
+  </span>
+</nav>
 
-{@render children()};
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<p class="bg-red-50">Hello world!</p>
+
+
+
+
 
 <button class="btn">Button</button>
 <button class="btn btn-neutral">Neutral</button>
 <button class="btn btn-primary">Primary</button>
 <button class="btn btn-secondary">Secondary</button>
 <button class="btn btn-accent">Accent</button>
-<button class="btn btn-ghost">Ghost</button>
 <button class="btn btn-link">Link</button>
-<button class="btn btn-square">
-  <span class="loading loading-spinner"></span>
-</button>
+<span class="loading loading-spinner"></span>
 
 <!-- To get more components like this: https://daisyui.com/components/button/ -->
